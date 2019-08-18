bbcfde48e335ecdeaaf51f269e380a0fed2535b1
Ari Stathopoulos <aristath@gmail.com>
2019-08-18 11:55:35 +0300

    cleanup

--- src/Telemetry.php
+++ src/Telemetry.php
@@ -12,8 +12,6 @@
 
 namespace Kirki\Util;
 
-use Kirki\Compatibility\Kirki;
-
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
--- src/Util.php
+++ src/Util.php
@@ -12,7 +12,6 @@
 
 namespace Kirki\Util;
 
-
 /**
  * Utility class.
  */
